f5ee383a9b1229c54eb10f0136457b37621cbf6d
Amany ElHwary <74727027+Amany-Reda@users.noreply.github.com>
2024-08-20 10:37:06 +0200

    Delete qataria_mod directory

--- qataria_mod/controllers/test_api.py
+++
@@ -1,6 +0,0 @@
-from odoo import http
-
-class TestApi(http.Controller):
-    @http.route('/test/api' , methods=["GET"], type='http' , auth='none' ,csrf=False)
-    def test_endpoint(self):
-        print("inside Test Api Method")
--- qataria_mod/models/stock_move.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class StockMove(models.Model):
-    _inherit = "stock.move"
-    # _order = 'sequence, id'
-
-    del_partner_id = fields.Many2one(related='picking_id.partner_id')
